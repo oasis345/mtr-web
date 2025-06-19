cf10cd94622012ff4a4063ce8f3a3be932b72eef
osh <dhtpgus7@gmail.com>
2025-06-19 21:04:39 +0900

    feat: 모노레포 설정

---
+++ apps/web/src/app/(before-login)/signin/page.tsx
@@ -0,0 +1,15 @@
+import { PageLayout, Section } from '@mtr/ui';
+
+const SigninPage = () => {
+  return (
+    <PageLayout variant="fullwidth">
+      <Section>
+        <div>
+          <h1>Signin</h1>
+        </div>
+      </Section>
+    </PageLayout>
+  );
+};
+
+export default SigninPage;
